ebd90129621def43522bc3a4e6d73e68b0794ab6
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-01 17:16:53 +0100

    script that starts a Flask web application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A module that representes a script that  starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """function to return the message to display in the user's browser"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """function to return the message to display on the /hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """function to display the message on the /c page"""
+    return "C" + " " + text.replace("_", " ")
+
+
+@app.route("python/<text>", strict_slashes)False)
+def python(text="is cool"):
+    """function to display message on the /python page"""
+    return "python" + " " + "text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
