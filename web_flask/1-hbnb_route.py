fd955e728da2c6b6ab7f71f96accb4442e67ea6f
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-01 16:54:23 +0100

    pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,12 @@ def hello():
     """function to return the message to display in the user's browser"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """function to return the message to display in the browser on the /hbnb page"""
+    """function to return the message to display on the /hbnb page"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
