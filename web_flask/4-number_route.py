974ab706565640b145a121baa05ba4eceaa572c5
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-01 17:41:31 +0100

    script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A module that representes a script that  starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """function to return the message to display in the user's browser"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """function to return the message to display on the /hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """function to display the message on the /c page"""
+    return "C" + " " + text.replace("_", " ")
+
+
+@app.route("/python")
+@app.route("/python/")
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """function to display message on the /python page"""
+    return "Python" + " " + text.replace("_", " ")
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(int n):
+    """display the message on the /number page"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
