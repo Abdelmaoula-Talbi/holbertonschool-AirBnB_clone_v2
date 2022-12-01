c9a366a2498a8829fa878ca78baafb2fb275eb5d
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-01 16:40:31 +0100

    fix message to display

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """function to return the message to display in the user's browser"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0")
