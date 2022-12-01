dc383f5eecda7230abce947fb41e87413513430f
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-01 17:03:58 +0100

    script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that starts a Flask web app
+"""
+A module that representes a script that  starts a Flask web application
 """
 
 from flask import Flask
@@ -7,20 +8,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
+    """function to return the message to display in the user's browser"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """function to return the message to display on the /hbnb page"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def C(text):
-    return "C " + text.replace("_", " ")
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """function"""
+    return "C" + " " + text.replace("_", " ")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
