73a8d5f045bd049ae85d54f942600ea060cd86f1
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2023-03-14 20:23:49 +0100

    task 2

--- console.py
+++ console.py
@@ -131,7 +131,7 @@ class HBNBCommand(cmd.Cmd):
                 if hasattr(new_instance, key):
                     value.replace("_", " ")
                     setattr(new_instance, key, value)
-            new_instance.save()
+            #new_instance.save()
             storage.new(new_instance)
             storage.save()
             print(new_instance.id)
