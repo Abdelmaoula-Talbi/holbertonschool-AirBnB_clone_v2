b0efead02d01e68f00396be7e086e0dffaef4d75
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2023-03-15 13:37:21 +0100

    add the storage save and new methods to the create function

--- console.py
+++ console.py
@@ -132,6 +132,7 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, key, value)
             new_instance.save()
             print(new_instance.id)
+            storage.new(new_instance)
             storage.save()
             
 
