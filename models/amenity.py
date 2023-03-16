7a5e682286dd9871835129278df99c07c8a8971f
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2023-03-16 19:06:27 +0100

    import Table from sqlalchemy

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
